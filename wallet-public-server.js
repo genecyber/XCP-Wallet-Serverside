b58e4773a173b8d1ff0851ba89ec4485cd9b8075
root <root@VA1PROTDOMAIN01.local>
2015-05-02 18:07:49 +0000

    adding server

--- wallet-public-server.js
+++ wallet-public-server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/site'));
+app.use(express.static(__dirname + '/XCP-Wallet-Webapp/Chrome%20Extension/'));
 
-app.get('/XCP-Wallet-Webapp/Chrome%20Extension/', function(req,res,err){
+app.get('/wallet', function(req,res,err){
     res.json({status:"success"})
 })
 
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3002);
